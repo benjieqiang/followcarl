b7d58b1f67f1766295ad49f2b0e1125b1551fda1
benjieqiang <765126548@qq.com>
2023-06-05 23:51:33 +0800

    fix: 重叠区间问题

--- src/main/java/com/ben/followcarl/c9_greedy/L11_406_reconstructQueue.java
+++ src/main/java/com/ben/followcarl/c9_greedy/L11_406_reconstructQueue.java
@@ -8,7 +8,10 @@ import java.util.LinkedList;
 /**
  * @Author: benjieqiang
  * @CreateTime: 2023-06-03  12:49
- * @Description: h是身高，k是前面有k个人身高大于h；
+ * @Description: h是身高，k是前面有k个人身高大于h；{7,0},{4,4},{7,1},{5,0},{6,1},{5,2}
+ * 如果按照k进行排序，发现h和k都不能确定，即第五个元素是[5,2],但是他前面有四个h大于或等于2的人，不满足。
+ * 而且此时，h身高也没有按照顺序排序。
+ * 所以需要按照h进行降序(因为看的是前面有多少人比我高)，相同身高h按照k从小到大排列，比如[7,0]和【7，1】肯定是后者在后面，所以由小到大排序；
  * @Version: 1.0
  */
 public class L11_406_reconstructQueue {
@@ -17,16 +20,21 @@ public class L11_406_reconstructQueue {
         // 从大到小排，则只需返回b - a; 从小到大排，返回a - b；
         Arrays.sort(people, (a, b) -> {
             if (a[0] == b[0]) {
-                System.out.println(a[1] - b[1]);
                 return a[1] - b[1];
             }
             return b[0] - a[0];
         });
-
+//        for (int[] num : people) {
+//            System.out.print(Arrays.toString(num) + ",");
+//        }
+        //此时已经排好序，[7, 0],[7, 1],[6, 1],[5, 0],[5, 2],[4, 4],[5, 0]
         LinkedList<int[]> que = new LinkedList<>();
+        // 这样插入的原理是啥？我们遍历整个people数组，每次看k的位置，依次加入[7, 0],[7, 1]
+        // 遇到[6,1]，必须得放到他两中间，因为我们的people数组已经按照h进行排序了，满足[7,1]前面只有一个比他高的元素
+        // void add(int index，Object element)：此方法在列表中的指定索引处插入元素。它将当前位于该位置的元素(如果有)和任何后续元素右移(将在其索引处增加一个)。
 
         for (int[] p : people) {
-            que.add(p[1],p); // 根据k的索引插入p到队列的对应位置；
+            que.add(p[1], p); // 根据k的索引插入p到队列的对应位置；
         }
 
         return que.toArray(new int[people.length][]);
@@ -36,7 +44,9 @@ public class L11_406_reconstructQueue {
     public void testRes() {
         int[][] people = {{7,0},{4,4},{7,1},{5,0},{6,1},{5,2}};
         int[][] res = reconstructQueue(people);
-        System.out.println(res);
+        for (int[] num : res) {
+            System.out.println(Arrays.toString(num));
+        }
     }
 
 }
--- src/main/java/com/ben/followcarl/c9_greedy/L13_435_eraseOverlapIntervals.java
+++ src/main/java/com/ben/followcarl/c9_greedy/L13_435_eraseOverlapIntervals.java
@@ -1,5 +1,7 @@
 package com.ben.followcarl.c9_greedy;
 
+import org.junit.Test;
+
 import java.util.Arrays;
 
 /**
@@ -14,12 +16,27 @@ public class L13_435_eraseOverlapIntervals {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0])); // 利用a - b会溢出
         int res = 0; // 记录重复区间，默认无重叠
         for (int i = 1; i < intervals.length; i++) {
-            // 统计重复区间：当前元素的左边界和上一个元素的右边界进行比较，如果小于，则是不重合的；等于和大于表示不重合。
+            // 不重叠情况：当前左边界 大于或等于 上一个元素右边界
+            // 统计重叠区间：如果小于，则是重合的；
             if (intervals[i][0] < intervals[i - 1][1]) {
                 res++;
+                // 为了记录i+1位置的区间和它上面的区间i和i-1的区间是否重叠。所以取了最小右边界；
+                // 为啥取最小右边界，因为右边界以前的区间i和i-1的部分是肯定重叠的。所以要比较三个区间的重叠必须取最小的；
                 intervals[i][1] = Math.min(intervals[i][1], intervals[i - 1][1]);
             }
         }
         return res;
+    }
+
+    @Test
+    public void testErase() {
+        int[][] intervals = {
+                {1,2},
+                {3,6},
+                {7,12},
+                {4,8},
+                {10,16}
+        };
+        System.out.println(eraseOverlapIntervals(intervals));
     }
 }
